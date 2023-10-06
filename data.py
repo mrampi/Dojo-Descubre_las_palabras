bb12c8c9051157a336ef03d369beac55f9757c7b
JuanArrua208 <108731993+JuanArrua208@users.noreply.github.com>
2023-10-06 09:13:26 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,12 +1,14 @@
 lista_palabras = {
-    "mares": [
+    "mares": 
+    [
         "remas", "resma", "mersa", "mesar", "meras", "mares", "armes", "ares", 
         "meas", "sera", "reas", "mesa", "rase", "raes", "eras", "sema", "mase", 
         "arme", "mera", "mear", "rema", "ames", "res", "esa", "sea", "mar", 
         "ser", "mas", "ras", "mes", "ase", "rae", "era", "rea", "mea", "are", "ame"
     ],
     
-    "relojeria": [
+    "relojeria": 
+    [
         "reloj", "alejo", "rojo", "jaleo", "lea", "ola", "rola", "real", "ole"
     ]
 }
